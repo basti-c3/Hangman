4c114f3c5bffb7a7546357dadce3c123b17007c6
Basti  Kreuz <basti.kreuz@googlemail.com>
2022-03-12 11:33:12 +0100

    5 - Build in a few fail safes - Raise exception when word in word_set has not same length as searched word, of when there are no words in the word_set - Return set with only searched word when there are no blanks _ left in already guessed word

--- Algorithm/get_possible_words.py
+++ Algorithm/get_possible_words.py
@@ -1,10 +1,18 @@
 from useful_things import *
 import re
+import random
 
 
 def get_possible_words(already_guessed_word: str, word_set: set):
+    if len(word_set) == 0:
+        raise Exception('There are no possible words left anymore. Should be impossible')
+    random_word_from_set = random.sample(word_set, 1)
+    if len(already_guessed_word) != len(random_word_from_set):
+        raise Exception(f'The length of the word {random_word_from_set} does not equal the length'
+                        f'of the searched word {already_guessed_word}')
     if '_' not in already_guessed_word:
         print('The word has nothing left to guess')
+        return set(already_guessed_word)
     alphabet_regex = '[A-Z]'
     regex = already_guessed_word.replace('_', alphabet_regex)
     return set(filter(lambda word: _word_contains_regex(word, regex), word_set))
